a62a8bb056a400f58363831ba24aaaaeb61eb0fb
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-06-24 15:34:19 -0300

    incluindo ejx

--- my-app/src/componentes/básicos/Primeiro.js
+++ my-app/src/componentes/básicos/Primeiro.js
@@ -1,3 +1,13 @@
+//para utilizar ejx dentro do componente precisa-se importar o react
+import React from 'react'
+
+
 export default function Primeiro(){
-    return 'Primeiro Componente'
+    const msg = 'Seja bem vindo(a)'
+    return (
+        <div>
+            <h2>Primeiro Componente</h2>
+            <p>{msg}</p>
+        </div>
+    )
 }
