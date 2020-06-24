a6b89c61d11c55bdd121759392ece2b23ec29b02
RicardoBaltazar <ricardo.baltazar04@gmail.com>
2020-06-24 15:27:12 -0300

    primeiro componente

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,9 +2,12 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-
+import Primeiro from './componentes/básicos/Primeiro'
+//import primeiro from './componentes/básicos/Primeiro'
 
 ReactDOM.render(
-    <div>Olá</div>
+    <div>
+        <Primeiro></Primeiro>
+    </div>
 , document.getElementById('root')
 )
